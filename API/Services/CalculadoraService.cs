a9abda90736bc77d90b21870e79f016b5dbbe7e8
Matias Raimondi <matiraimondi1@gmail.com>
2025-10-15 20:36:00 -0300

    Hecho en clase.

---
+++ API/Services/CalculadoraService.cs
@@ -0,0 +1,35 @@
+﻿using API.Protos;
+using Grpc.Core;
+
+namespace API.Services
+{
+    public class CalculadoraService : Calculadora.CalculadoraBase
+    {
+        public override Task<Response> Sumar(Request request, ServerCallContext context)
+        {
+            var result = request.Number1 + request.Number2;
+            return Task.FromResult(new Response { Result = result });
+        }
+
+        public override Task<Response> Restar(Request request, ServerCallContext context)
+        {
+            var result = request.Number1 - request.Number2;
+            return Task.FromResult(new Response { Result = result });
+        }
+
+        public override Task<Response> Multiplicar(Request request, ServerCallContext context)
+        {
+            var result = request.Number1 * request.Number2;
+            return Task.FromResult(new Response { Result = result });
+        }
+
+        public override Task<Response> Dividir(Request request, ServerCallContext context)
+        {
+            if (request.Number2 == 0)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "No se puede dividir por cero"));
+
+            var result = request.Number1 / request.Number2;
+            return Task.FromResult(new Response { Result = result });
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using API.Protos;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7208");
+            var client = new Calculadora.CalculadoraClient(channel);
+
+            var sumarResponse = client.Sumar(new Request { Number1 = 10, Number2 = 5 });
+            Console.WriteLine($"Suma: {sumarResponse.Result}");
+            var restarResponse = client.Restar(new Request { Number1 = 10, Number2 = 5 });
+            Console.WriteLine($"Resta: {restarResponse.Result}");
+            var multiplicarResponse = client.Multiplicar(new Request { Number1 = 10, Number2 = 5 });
+            Console.WriteLine($"Multiplicación: {multiplicarResponse.Result}");
+            var dividirResponse = client.Dividir(new Request { Number1 = 10, Number2 = 5 });
+            Console.WriteLine($"División: {dividirResponse.Result}");
+
+            Console.ReadKey();
+        }
+    }
+}
